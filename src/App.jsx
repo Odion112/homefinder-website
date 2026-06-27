0654dd48d63a97941e03f861eab5f6b1a5489034
Odion Okhuevbie <okhuevbieodion@gmail.com>
2026-06-27 17:22:12 +0100

    input field completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import ProfileModal from "./components/ProfileModal";
+import Input from "./components/Input";
 
 function App() {
-  const [profileOpen, setProfileOpen] = useState(false);
-
+  
   return (
     <>
-      <ProfileModal isOpen={profileOpen} onClose={() => setProfileOpen(false)} />
+      <Input />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/properties" element={<Properties />} />
       </Routes>
     </>
