87b9072b3886722ba2f2f5dc291f53db33baa913
Odion Okhuevbie <okhuevbieodion@gmail.com>
2026-06-27 17:36:47 +0100

    confirm dialog completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import Input from "./components/Input";
+import ConfirmDialog from "./components/ConfirmDialog";
 
 function App() {
   
   return (
     <>
-      <Input />
+      <ConfirmDialog />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/properties" element={<Properties />} />
