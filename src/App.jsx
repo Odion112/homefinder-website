10ff7e231f4b7221ebd84192244fdb8ef7b07c5d
Odion Okhuevbie <okhuevbieodion@gmail.com>
2026-06-28 18:12:42 +0100

    owner setup form completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import Navbar from "./components/Navbar";
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+
 
 function App() {
   
   return (
     <>
-
-    <SignUp />
 
       <Routes>
         <Route path="/" element={<Home />} />
