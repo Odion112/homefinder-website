9a2a0e8a3d695ae2445ba34486d7ffed54801b38
Odion Okhuevbie <88948242+Odion112@users.noreply.github.com>
2026-06-28 16:12:55 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import Navbar from "./components/Navbar";
 
 function App() {
-  
   return (
-    <>
-
-    <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/properties" element={<Properties />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/properties" element={<Properties />} />
+    </Routes>
   );
 }
 
