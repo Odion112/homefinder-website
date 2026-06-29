c13f8a37dde178fc7768dde7e2579bed52349e39
Odion Okhuevbie <88948242+Odion112@users.noreply.github.com>
2026-06-29 05:06:45 +0100

    Update
    Removed ExistingOwnerList component from App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-import ExistingOwnerList from "./pages/listing/ExistingOwnerList";
 
 
 function App() {
   return (
     <>
-
-    <ExistingOwnerList />
 
       <Routes>
         <Route path="/" element={<Home />} />
