5ef5dea5df310cab3766f0e14a6e6580f686c4cb
Odion Okhuevbie <okhuevbieodion@gmail.com>
2026-06-29 13:31:48 +0100

    Property details page update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
 import ExistingOwnerList from "./pages/listing/ExistingOwnerList";
-
+import PropertyDetails from "./pages/PropertyDetails";
 
 function App() {
   return (
     <>
 
-    <ExistingOwnerList />
+    <PropertyDetails />
+
 
       <Routes>
         <Route path="/" element={<Home />} />
