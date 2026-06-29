00c65ed06773d916e942a84a43f7e6fb84b0baf5
Odion Okhuevbie <okhuevbieodion@gmail.com>
2026-06-29 06:44:03 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
-
+import OwnerSetupPage from "./pages/listing/OwnerSetup";
+import ListProperty from "./pages/listing/ListProperty";
 
 function App() {
   return (
-    <>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/properties" element={<Properties />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/properties" element={<Properties />} />
+      <Route path="/list-property" element={<OwnerSetupPage />} />
+      <Route path="/list-property/details" element={<ListProperty />} />
+    </Routes>
   );
 }
 
